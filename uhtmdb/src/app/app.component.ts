b73c1e928d1d06f33bbeed04506bcfad2834f915
geralaptop <s20120240@alumnos.itsur.edu.mx>
2025-03-19 21:22:55 -0600

    added float button

--- uhtmdb/src/app/app.component.ts
+++ uhtmdb/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [ 
-    NavigationBarComponent
+    NavigationBarComponent,
+    MatIconModule,
+    MatButtonModule
    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
